e9527f33314f25a13bfddb251d24b75653b69a65
Hendrik Geßner <HENGE01@gmail.com>
2017-07-07 13:11:04 +0200

    Created ContextDescriptions.

---
+++ www/js/pmodules/context/ContextDescriptions.js
@@ -0,0 +1,68 @@
+define([
+    'contactJS'
+], function (contactJS) {
+
+    var contextDescriptions = {
+        CI_DEVICE_INFO: {
+            name: "CI_DEVICE_INFO",
+            type: "OBJECT",
+            parameterList: []
+        },
+        CI_ATTENDED_COURSES: {
+            name: "CI_ATTENDED_COURSES",
+            type: "ARRAY",
+            parameterList: []
+        },
+        CI_CURRENTLY_ATTENDED_COURSES: {
+            name: "CI_CURRENTLY_ATTENDED_COURSES",
+            type: "ARRAY",
+            parameterList: []
+        },
+        CI_CURRENTLY_RUNNING_COURSES: {
+            name: "CI_CURRENTLY_RUNNING_COURSES",
+            type: "ARRAY",
+            parameterList: []
+        },
+        CI_CURRENT_DATETIME: {
+            name: "CI_CURRENT_DATETIME",
+            type: "DATE",
+            parameterList: [["CP_UNIT", "STRING", "ISO 8601"]]
+        }
+    };
+
+    var ContextDescriptions = function () {
+        this._discoverer = new contactJS.Discoverer();
+    };
+
+    /**
+     * Retrieves the raw context information description as used by widgets and interpreters if available. Throws an error if the given name is not found
+     * @param {String} contextName Common name of the context information description
+     * @returns {Object} context Context information description as a raw object
+     * @returns {String} context.name
+     * @returns {String} context.type
+     * @returns {Array} context.parameterList
+     * @throws error if the contextName is not known
+     */
+    ContextDescriptions.prototype.getRaw = function (contextName) {
+        var result = contextDescriptions[contextName];
+        if (!result) {
+            if (console.error) console.error("The requested contextName is not available");
+            throw "The requested contextName is not available";
+        } else {
+            return result;
+        }
+    };
+
+    /**
+     * Retrieves the context information description as used by rules if available. Throws an error if the given name is not found
+     * @param {String} contextName Common name of the context information description
+     * @returns {ContextInformation} context Context information description
+     * @throws error if the contextName is not known
+     */
+    ContextDescriptions.prototype.get = function (contextName) {
+        var result = this.getRaw(contextName);
+        return contactJS.ContextInformation.fromContextInformationDescription(this._discoverer, result);
+    };
+
+    return new ContextDescriptions();
+});
