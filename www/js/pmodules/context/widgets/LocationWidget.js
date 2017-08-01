562ee1e0968344b5b8033c855faebecb5d7de017
Hendrik Geßner <HENGE01@gmail.com>
2017-08-01 19:44:03 +0200

    Integrated Background Geolocation into LocationWidget.

--- www/js/pmodules/context/widgets/LocationWidget.js
+++ www/js/pmodules/context/widgets/LocationWidget.js
@@ -25,6 +25,63 @@ define([
         updateInterval: 1000 * 30 // twice per minute
     };
 
+    LocationWidget.prototype.startGeoLocation = function() {
+        // Get a reference to the plugin.
+        var bgGeo = window.BackgroundGeolocation;
+
+        //This callback will be executed every time a geolocation is recorded in the background.
+        var callbackFn = _.bind(function(location, taskId) {
+            this.lastKnownLocation = location;
+            console.log('- Location: ', JSON.stringify(location));
+            // Must signal completion of your callbackFn.
+            bgGeo.finish(taskId);
+        }, this);
+
+        // This callback will be executed if a location-error occurs.  Eg: this will be called if user disables location-services.
+        var failureFn = _.bind(function(errorCode) {
+            console.warn('- BackgroundGeoLocation error: ', errorCode);
+        }, this);
+
+        bgGeo.on('location', callbackFn, failureFn);
+        bgGeo.on('heartbeat', _.bind(function(params) {
+            this.lastKnownLocation = params.location;
+            console.log('- hearbeat');
+        }, this));
+
+        // BackgroundGeoLocation is highly configurable.
+        bgGeo.configure({
+            // Common Options
+            desiredAccuracy: 1000,
+            distanceFilter: 100,
+            stopOnTerminate: false,
+            startOnBoot: true,
+            // iOS options
+            useSignificantChangesOnly: true,
+            preventSuspend: true,
+            // Android Options
+            foregroundService: true,
+            // Logging & Debug Options
+            debug: true,  // <-- Debug sounds & notifications.
+
+
+
+            stationaryRadius: 25,
+
+            // Activity Recognition config
+            activityRecognitionInterval: 10000,
+            stopTimeout: 5,
+            // Application config
+
+
+        }, function(state) {
+            // This callback is executed when the plugin is ready to use.
+            console.log("BackgroundGeolocation ready: ", state);
+            if (!state.enabled) {
+                bgGeo.start();
+            }
+        });
+    };
+
     LocationWidget.prototype.queryGenerator = function(callback) {
         var finish = _.bind(function(result) {
             var response = new contactJS.ContextInformationList();
@@ -32,6 +89,19 @@ define([
             this._sendResponse(response, callback);
         }, this);
 
+        if (!this.geoLocationStarted) {
+            this.startGeoLocation();
+            this.geoLocationStarted = true;
+        }
+
+        var location = this.lastKnownLocation;
+        if (location) {
+            finish(location);
+        } else {
+            finish(contactJS.ContextInformation.VALUE_UNKNOWN);
+        }
+
+        /*
         // TODO: Implement
         var result = {
             coords: {
@@ -47,6 +117,7 @@ define([
         };
 
         finish(result);
+        */
     };
 
     return LocationWidget;
