b8c7059378e942cd2a5cb9489954ba2a8b0adab7
Hendrik Geßner <HENGE01@gmail.com>
2017-08-22 16:44:27 +0200

    Changed location sending to constant rate.

--- www/js/pmodules/context/rules/Debug.js
+++ www/js/pmodules/context/rules/Debug.js
@@ -11,23 +11,11 @@ define([
 
     return [
         {
-            id: "28afdeb1-6fdd-464b-b03d-5526d1cc1c08",
-            relatedContextInformation: [context.location],
-            condition: function(R) {
-                R.when(true);
-            },
-            consequence: function(R) {
-                new StatementSender().sendHeartbeat();
-                R.next();
-            }
-        },
-        {
             id: "0a917528-befb-470e-bfa4-96e0580d6cdf",
             relatedContextInformation: [context.location],
             condition: function(R) {
-                var older = context.location.isOlderThan("DEBUG_USER_LOCATION", 60*1000, this);
-                var different = context.location.isDifferentFromLastValue("DEBUG_USER_LOCATION", this);
-                R.when(older || different);
+                // Send values older than 5 minutes
+                R.when(context.location.isOlderThan("DEBUG_USER_LOCATION", 5*60*1000, this));
             },
             consequence: function(R) {
                 var value = context.location.updateLastValue("DEBUG_USER_LOCATION", this);
