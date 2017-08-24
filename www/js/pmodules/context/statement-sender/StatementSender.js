de9c066fb0416a37036ffdf4720c9d603f6dc651
Hendrik Geßner <HENGE01@gmail.com>
2017-08-24 20:30:58 +0200

    Moved profile related sender code into ProfileSender.

--- www/js/pmodules/context/statement-sender/StatementSender.js
+++ www/js/pmodules/context/statement-sender/StatementSender.js
@@ -6,66 +6,9 @@ define([
     'Session',
     './BaseSender',
     './HeartbeatSender',
-    './CourseSender'
+    './CourseSender',
+    './ProfileSender'
 ], function(xapi, activities, verbs, context, Session, StatementSender) {
-
-    /**
-     *
-     * @param device
-     * @param {string} device.uuid Device identifier
-     * @param {string} device.language Device language in short form, e.g. "en-US" or "de-DE"
-     * @param {string[]} device.phoneNumbers Device phone numbers
-     */
-    StatementSender.prototype.sendDeviceDetails = function(device) {
-        var extensions = {};
-        if (device.language) {
-            extensions.languageCode = device.language;
-        }
-        if (device.phoneNumbers) {
-            extensions.phoneNumbers = device.phoneNumbers;
-        }
-
-        var stmt = new xapi.ADL.XAPIStatement(
-            this._agent,
-            verbs.used,
-            activities.device(device.uuid, extensions)
-        );
-
-        this._sendStatement(stmt);
-    };
-
-    /**
-     *
-     * @param location current user location
-     * @param {number} location.latitude Latitude
-     * @param {number} location.longitude Longitude
-     * @param {number} location.accuracy Accuracy in meters
-     */
-    StatementSender.prototype.sendUserLocation = function(location) {
-        var stmt = new xapi.ADL.XAPIStatement(
-            this._agent,
-            verbs.wasat,
-            activities.place(location)
-        );
-
-        this._sendStatement(stmt);
-    };
-
-    /**
-     *
-     * @param service
-     * @param {string} service.provider "mail", "facebook" etc., must be known
-     * @param {string} service.identity User name at the given provider
-     */
-    StatementSender.prototype.sendServiceLogin = function(service) {
-        var stmt = new xapi.ADL.XAPIStatement(
-            this._agent,
-            verbs.loggedin,
-            activities.service(service.provider, service.identity)
-        );
-
-        this._sendStatement(stmt);
-    };
 
     return StatementSender;
 });
